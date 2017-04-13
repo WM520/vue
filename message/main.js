c6247e9c408e66b21b8ec0717730921c1c915cce
WM520 <1071597946@qq.com>
2017-04-13 12:01:22 +0800

    组件绑定数据

--- message/main.js
+++ message/main.js
@@ -1,3 +1,18 @@
 /**
  * Created by wm on 13/04/2017.
  */
+
+Vue.component('message',{
+
+    props:['title', 'body'],
+
+
+    template: `
+         <article class="message"> <div class="message-header"> <p>{{ title }}</p> <div class="message-body">{{body}} </div> </article>
+  `,
+});
+
+
+new Vue ({
+    el: '#app'
+})
