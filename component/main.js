a2397d9cb3adc1a5e19bdafcfdcf0c00edae1844
WM520 <1071597946@qq.com>
2017-04-13 11:10:04 +0800

    components

--- component/main.js
+++ component/main.js
@@ -1,9 +1,27 @@
 /**
  * Created by wm on 13/04/2017.
  */
-Vue.component('task',{
-    template: '<li>football</li>'
+
+Vue.component('task-list', {
+    template: '<div><li v-for="tast in tasks">{{ tast.tast }}</li></div>',
+    data(){
+        return {
+            tasks : [
+                {tast: 'wangmiao', complete: true},
+                {tast: 'wangmiao', complete: true},
+                {tast: 'wangmiao', complete: true}
+            ]
+        };
+
+    }
 });
-new Vue ({
+
+Vue.component('task',{
+    template: '<li><slot></slot></li>'
+});
+
+
+
+var vm = new Vue ({
     el:'#app'
-})
+});
